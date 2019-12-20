1603e1c7a259d436913080c181902de63e55eae6
mikeblazanin <mikeblazanin@gmail.com>
2019-12-19 21:29:48 -0500

    Wrote first draft of first half of new find_extrema function

--- Analyze_Data.R
+++ Analyze_Data.R
@@ -121,15 +121,15 @@ for (i in 1:nrow(gc_data)) {
 #reorder
 gc_data <- gc_data[order(gc_data$uniq_well, gc_data$Time_s), ]
 
-calc_deriv <- function(CFU, percapita = FALSE,
+calc_deriv <- function(density, percapita = FALSE,
                           subset_by = NULL, time = NULL,
                           time_normalize = NULL) {
-  #Note! CFU values must be sorted sequentially into their unique sets already
+  #Note! density values must be sorted sequentially into their unique sets already
   
-  #Provided a vector of CFU values, this function returns (by default) the
+  #Provided a vector of density values, this function returns (by default) the
   # difference between sequential values
-  #if percapita = TRUE, the differences of cfu are divided by cfu
-  #if subset_by is provided, it should be a vector (same length as cfu),
+  #if percapita = TRUE, the differences of density are divided by density
+  #if subset_by is provided, it should be a vector (same length as density),
   # the unique values of which will separate calculations
   #if time_normalize is specified, time should be provided as a simple 
   # numeric (e.g. number of seconds) in some unit
@@ -150,10 +150,10 @@ calc_deriv <- function(CFU, percapita = FALSE,
   }
   
   #Calc derivative
-  ans <- c(CFU[2:length(CFU)]-CFU[1:(length(CFU)-1)])
+  ans <- c(density[2:length(density)]-density[1:(length(density)-1)])
   #Percapita (if specified)
   if (percapita) {
-    ans <- ans/CFU[1:(length(CFU)-1)]
+    ans <- ans/density[1:(length(density)-1)]
   }
   #Time normalize (if specified)
   if (!is.null(time_normalize)) {
@@ -169,15 +169,11 @@ calc_deriv <- function(CFU, percapita = FALSE,
 
 #Smooth data
 gc_data$sm_loess <- NA
-gc_data$percap_deriv_sm_loess <- NA
-gc_data$percap_deriv_cfu <- NA
 for (my_well in unique(gc_data$uniq_well)) {
   my_rows <- which(gc_data$uniq_well == my_well)
   #Smooth with loess
-  gc_data$sm_loess[my_rows] <- predict(loess(cfu_ml ~ Time_s,
-                                             span = 0.4,
-                                             data = gc_data[my_rows, ]),
-                                       gc_data[my_rows, ])
+  gc_data$sm_loess[my_rows] <- loess(cfu_ml ~ Time_s,span = 0.4,
+                                             data = gc_data[my_rows, ])$fitted
 }
 
 #Calculate growth per hour from loess curve
@@ -203,7 +199,7 @@ gc_data$percap_deriv_cfu <- calc_deriv(gc_data$cfu_ml,
 
 #View samples of original & smoothed curves
 # as well as derivatives (per cap & not) of both orig and smoothed curves
-for (my_well in sample(unique(gc_data$uniq_well), 20)) {
+for (my_well in sample(unique(gc_data$uniq_well), 5)) {
   my_rows <- which(gc_data$uniq_well == my_well)
   
   print(cowplot::plot_grid(
@@ -233,6 +229,242 @@ for (my_well in sample(unique(gc_data$uniq_well), 20)) {
 #   (pseudo carrying capacity)
 # find time from some low density to peak per capita growth rate (pseudo lag time)
 # find time from max growth rate to pseudo carrying capacity
+
+find_local_extrema <- function(values, 
+                               return_maxima = TRUE,
+                               return_minima = FALSE,
+                               width_limit = NULL,
+                               height_limit = NULL,
+                               subset = NULL) {
+  #Takes a vector of values and returns a vector of the indices
+  # of all local value extrema (by default, returns all extrema)
+  # To only return maxima or minima, change return_maxima/return_minima to FALSE
+
+  #Either width_limit or height_limit must be provided
+  #Width is how wide the window should be to look for a peak
+  # Narrower bandwidth will be more sensitive to narrow local maxima
+  # Wider bandwidth will be less sensitive to narrow local maxima
+  #Height is how deep of a valley the function will search from each local
+  # maxima to find other local maxima
+
+  #Subset can be a Boolean vector same length as density & time
+  # with TRUE as values that should be included
+  # or it can be a vector of the indices of density & time that should be used
+  
+  #This function is designed to be run with dplyr::group_by and summarize
+  
+  #Check inputs
+  if (!return_maxima & !return_minima) {
+    stop("Both return_maxima and return_minima are FALSE, at least one must be TRUE")
+  }
+  if (is.null(width_limit) & is.null(height_limit)) {
+    stop("Either width_limit or height_limit must be provided")
+  } else if (!is.null(width_limit) & !is.null(height_limit)) {
+    stop("Both width_limit and height_limit are provided, currently only support for one at a time is implemented")
+  }
+  
+  #Start finding peaks
+  maxima_list <- c()
+  minima_list <- c()
+  
+  #use width limit to find peaks
+  if (!is.null(width_limit)) { 
+    ##Check for first maxima
+    cnt_pos <- 1
+    best_pos <- cnt_pos+1 #arbitrary index != to cnt_pos
+    while (cnt_pos != best_pos) {
+      #Move the previous best pointer to current pointer location
+      best_pos <- cnt_pos
+      #Then move current pointer to highest point within window
+      # (making sure not to check non-integer indices, or indices below 1 or
+      #  higher than the length of the vector)
+      cnt_pos <- which.max(values[max(c(1, cnt_pos-floor(width_limit/2))):
+                              min(c(length(values), cnt_pos+floor(width_limit/2)))])
+    }
+    #add value to maxima_list
+    maxima_list <- c(maxima_list, best_pos)
+    
+    ##Check for first minima
+    cnt_pos <- 1
+    best_pos <- cnt_pos+1 #arbitrary index != to cnt_pos
+    while (cnt_pos != best_pos) {
+      #Move the previous best pointer to current pointer location
+      best_pos <- cnt_pos
+      #Then move current pointer to highest point within window
+      # (making sure not to check non-integer indices, or indices below 1 or
+      #  higher than the length of the vector)
+      cnt_pos <- which.min(values[max(c(1, cnt_pos-floor(width_limit/2))):
+                                    min(c(length(values), cnt_pos+floor(width_limit/2)))])
+    }
+    #add value to minima_list
+    minima_list <- c(minima_list, best_pos)
+    
+    ##Check for next extrema until we no longer find new ones
+    while (TRUE) {
+      #Since maxima & minima must alternate, always start with furthest one 
+      # we've found so far
+      cnt_pos <- max(c(minima_list, maxima_list))
+      #Assign best_pos to arbitrary valid index
+      best_pos <- if (cnt_pos != length(values)) {
+        best_pos <- cnt_pos+1
+      } else {best_pos <- cnt_pos-1}
+      #we're looking for a maxima next
+      if (cnt_pos %in% minima_list) { 
+        while (best_pos != cnt_pos) {
+          #Move the previous best pointer to current pointer location
+          best_pos <- cnt_pos
+          #Then move current pointer to highest point within window
+          # (making sure not to check non-integer indices, or indices below 1 or
+          #  higher than the length of the vector)
+          cnt_pos <- which.max(values[max(c(1, cnt_pos-floor(width_limit/2))):
+                                        min(c(length(values), cnt_pos+floor(width_limit/2)))])
+        }
+        #add value to maxima_list
+        maxima_list <- c(maxima_list, best_pos)
+      #we're looking for a minima next
+      } else if (cnt_pos %in% maxima_list) { 
+        while (best_pos != cnt_pos) {
+          #Move the previous best pointer to current pointer location
+          best_pos <- cnt_pos
+          #Then move current pointer to highest point within window
+          # (making sure not to check non-integer indices, or indices below 1 or
+          #  higher than the length of the vector)
+          cnt_pos <- which.min(values[max(c(1, cnt_pos-floor(width_limit/2))):
+                                        min(c(length(values), cnt_pos+floor(width_limit/2)))])
+        }
+        #add value to minima_list
+        minima_list <- c(minima_list, best_pos)
+      }
+      if (best_pos %in% minima_list | best_pos %in% maxima_list) {break}
+    }
+  #use height limit to find peaks
+  } else if (!is.null(height_limit)) {
+    ##Check for first maxima
+    cnt_pos <- 1
+    best_pos <- cnt_pos+1 #arbitrary index != to cnt_pos
+    while (cnt_pos != best_pos) {
+      #Move the previous best pointer to current pointer location
+      best_pos <- cnt_pos
+      #Determine start and end of window
+      #Then move current pointer to highest point within window
+      # (making sure not to check non-integer indices, or indices below 1 or
+      #  higher than the length of the vector)
+      cnt_pos <- which.max(values[max(c(1, cnt_pos-floor(width_limit/2))):
+                                    min(c(length(values), cnt_pos+floor(width_limit/2)))])
+    }
+    #add value to maxima_list
+    maxima_list <- c(maxima_list, best_pos)
+    
+    #Check for first minima
+    
+    #Check for next extrema on loop
+  }
+  
+  
+  
+  
+  
+  #Check for first maxima
+  while (cnt_pos != best_pos) {
+    cnt_pos <- 
+  
+  
+  
+  #Walk through points to find first local maxima & first local minima
+  #whichever is earlier, start from there and find the next opposite type
+  #repeat, alternating searching for maxima or minima, saving indices along the way
+  #at end, put the types they requested output into an ordered list
+  
+  #Check by width:
+    #Order all points by height
+    #Find highest point
+    #Find highest point not within width
+    #Find highest point not within widths of #1 or #2
+    #Repeat until...
+  
+  #Check by height:
+  # Order all points by height
+  # Find highest point
+  # Determine closest points to peak that fall below height limit
+  # Find highest point outside of that width
+  #   repeat finding width essentially where it doesn't fall below height limit
+#   Repeat
+  
+  #Make sure to remove the first and last points, if they're included
+  ##and check for duplicates
+  
+  return(list("density" = ans_density, "time" = ans_time))
+}
+                               
+  
+  analyze_curves <- function(od_data, time_data, bandwidth = 10, return) {
+    #Takes vectors of the od_data and time_data
+    #Bandwidth is how wide the window should be to look for a peak
+    #Narrower bandwidth will get you an earlier local maxima
+    #Wider bandwidth will get you a later more-global maxima
+    #Designed to be run w/ group_by
+    prev_max_pos <- 0
+    cnt_max_pos <- bandwidth
+    while (cnt_max_pos != prev_max_pos) {
+      prev_max_pos <- cnt_max_pos
+      search_start <- pmax(cnt_max_pos-bandwidth, 1) #start of the search window
+      search_end <- pmin(search_start + 2*bandwidth, length(od_data)) #end of the search window
+      cnt_max_pos <- which.max(od_data[search_start:search_end]) + search_start - 1
+    }
+    if (return == "max") {return(od_data[cnt_max_pos])
+    } else if (return == "maxtime") {return(time_data[cnt_max_pos])}
+  }
+
+
+#Plot curves & peaks
+view_peaks <- function(time_data, dens_data, well_contents, plt_point = TRUE,
+                       peak_time = NULL, peak_dens = NULL, peak_well_contents,
+                       numplots = 9, lwd = 1) {
+  #Inputs:  time_data - vector of time values
+  #         dens_data - vector of density values, matched with time_data
+  #         well_contents - vector of well contents, matched with time_data
+  #         peak_time - vector of peaks' time values
+  #         peak_dens - vector of peaks' density values, matched with peak_time
+  #         peak_well_contents - vectore of peaks' well contents, matched with peak_time
+  require(ggplot2)
+  for (start_group in seq(from = 1, to = length(unique(well_contents)),
+                          by = numplots)) {
+    my_wells <- unique(well_contents)[start_group:(start_group+numplots-1)]
+    myplot <- ggplot(data = data.frame(
+      "dens" = dens_data[well_contents %in% my_wells],
+      "time" = time_data[well_contents %in% my_wells],
+      "contents" = well_contents[well_contents %in% my_wells]),
+      aes(x = time, y = dens)) +
+      geom_line(lwd = lwd) +
+      facet_wrap(~contents)
+    if (plt_point) {
+      myplot <- myplot + 
+        geom_point(data = data.frame(
+          "dens" = peak_dens[peak_well_contents %in% my_wells],
+          "time" = peak_time[peak_well_contents %in% my_wells],
+          "contents" = peak_well_contents[peak_well_contents %in% my_wells]),
+          aes(x = time, y = dens),
+          size = 3, pch = 13)
+    }
+    print(myplot)
+  }
+}
+
+view_peaks(tidymerge$Timepoint, tidymerge$OD, tidymerge$Well,
+           peak_time = outmerge$maxtime, peak_dens = outmerge$max, 
+           peak_well_contents = outmerge$Well)
+
+#Group data by indiv growth curves
+grp_data1 <- dplyr::group_by(tidycurves[!is.na(tidycurves$smoothed), ], 
+                             Well)
+
+#Get OD peak height & time for each growth curve
+out_data1 <- dplyr::summarize(grp_data1, 
+                              max = analyze_curves(smoothed, Timepoint, 
+                                                   bandwidth = 9, return = "max"),
+                              maxtime = analyze_curves(smoothed, Timepoint, 
+                                                       bandwidth = 9, 
+                                                       return = "maxtime"))
 
 
 
