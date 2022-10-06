eb90082212921573ec77f8f4124db5b603ab1a71
mikeblazanin <mikeblazanin@gmail.com>
2022-10-06 11:08:51 -0400

    Revert "Changed headers in lines 72ish to be correct (Mathematica also auto added a bunch of spaces, thus all the other line changes)"
    This reverts commit 9a0efcf797ddaa0f08459aa5144cf905b3a95166.

--- Modeling/Analysis/AnalyzeWave_I_Chi.m
+++ Modeling/Analysis/AnalyzeWave_I_Chi.m
@@ -1,81 +1,79 @@
-(* ::Package:: *)
-
 %% Load Wave Data
 clear all
 close all
 
-% FileList = dir(' ./OutputsIChi/* . mat');
-FileList = dir('../Outputs/OutputsIChi/* . mat');
+% FileList = dir('./OutputsIChi/*.mat');
+FileList = dir('../Outputs/OutputsIChi/*.mat');
 
-% Store measurements (PoplationCell 1, Pop . Cell 2, relative Chi,
-% relative i)
+%Store measurements (PoplationCell 1, Pop. Cell 2, relative Chi,
+%relative i)
 Measurements = zeros(length(FileList), 6); 
 
 for ii = 1:length(FileList)
-    File = ['../Outputs/OutputsIChi/', FileList (ii) . name];
+    File = ['../Outputs/OutputsIChi/', FileList(ii).name];
     load(File)
 
     %% Get wave at last timepoint
-    rho_cell _end = rho_cell _store(end, :);
-    rho_cell2 _end = rho_cell2 _store(end, :);
+    rho_cell_end = rho_cell_store(end, :);
+    rho_cell2_end = rho_cell2_store(end, :);
 
     % set(gca', 'yscale', 'log')
 
     %% Get indices of the wave
-    % Find the peaks and start of wave 1
-    peaks = findpeaks(rho_cell _end);
-    peakInd = find(rho_cell _end == peaks(end));
-    WaveStartInd = max(find(rho_cell _end(1:peakInd) <= min(rho_cell _end(1:peakInd))));
+    %Find the peaks and start of wave 1
+    peaks = findpeaks(rho_cell_end);
+    peakInd = find(rho_cell_end == peaks(end));
+    WaveStartInd = max(find(rho_cell_end(1:peakInd) <= min(rho_cell_end(1:peakInd))));
 
-    % Find peaks and start of wave 2
-    peaks2 = findpeaks(rho_cell2 _end);
-    peakInd2 = find(rho_cell2 _end == peaks2(end));
-    WaveStartInd2 = max(find(rho_cell2 _end(1:peakInd2) <= min(rho_cell2 _end(1:peakInd))));
+    %Find peaks and start of wave 2
+    peaks2 = findpeaks(rho_cell2_end);
+    peakInd2 = find(rho_cell2_end == peaks2(end));
+    WaveStartInd2 = max(find(rho_cell2_end(1:peakInd2) <= min(rho_cell2_end(1:peakInd))));
 
 %     figure();
-%     plot(rho_cell _end)
+%     plot(rho_cell_end)
 %     hold on
 %     xline(peakInd)
 %     xline(WaveStartInd)
 % 
-%     plot(rho_cell2 _end)
+%     plot(rho_cell2_end)
 %     xline(peakInd2)
 %     xline(WaveStartInd2)
 
     %% Sum cell density in the wave
-    cell_population = sum(rho_cell _end(WaveStartInd:end));
-    cell2_population = sum(rho_cell2 _end(WaveStartInd:end));
+    cell_population = sum(rho_cell_end(WaveStartInd:end));
+    cell2_population = sum(rho_cell2_end(WaveStartInd:end));
     
     %% Get Parameters
-    relativeChi = SimParams . Chi2 ./ SimParams . Chi;
-    RelativeI = SimParams . irate2 ./ SimParams . irate;
+    relativeChi = SimParams.Chi2 ./ SimParams.Chi;
+    RelativeI = SimParams.irate2 ./ SimParams.irate;
     
     %% Store measurements
-    Measurements(ii, :) = [cell_population, cell2_population, relativeChi, RelativeI, SimParams . Chi, SimParams . irate];
+    Measurements(ii, :) = [cell_population, cell2_population, relativeChi, RelativeI, SimParams.Chi, SimParams.irate];
 end
 
 %% Plot
 relChi = Measurements(:, 3);
 relI = Measurements(:, 4);
-relPopulation = Measurements (:, 2) ./ Measurements(:, 1);
+relPopulation = Measurements(:, 2) ./ Measurements(:, 1);
 
 
 figure()
 scatter(relChi, relI, 60, log(relPopulation), 'filled')
 a = colorbar;
 colormap jet
-a . Label . String = 'log N1/N2';
-a . Label . Rotation = -90;
+a.Label.String = 'log N1/N2';
+a.Label.Rotation = -90;
 xlabel("Relative Chi")
 ylabel("Relative I")
 set(gca,'xscale', 'log', 'yscale', 'log')
-saveas(gcf, ' ./I_vs_Chi . png')
+saveas(gcf, './I_vs_Chi.png')
 
 %% Export Data
-headers = ["Cell_population", "Cell2_population", "relativeChi", "relativeI", "Chi", "irate"];
+headers = ["Cell_population", "Cell2_population", "relativecA", "relativeI", "Chi", "irate"];
 textHeader = strjoin(headers, ',');
-fid = fopen(' ./I_vs_Chi . csv', 'w');
-fprintf(fid,'% s \n',textHeader)
+fid = fopen('./I_vs_Chi.csv', 'w');
+fprintf(fid,'%s\n',textHeader)
 fclose(fid)
 
-dlmwrite(' ./I_vs_Chi . csv',Measurements,'-append');
+dlmwrite('./I_vs_Chi.csv',Measurements,'-append');
--- Modeling/Analysis/AnalyzeWave_I_cR.m
+++ Modeling/Analysis/AnalyzeWave_I_cR.m
@@ -1,80 +1,78 @@
-(* ::Package:: *)
-
 %% Load Wave Data
 clear all
 close all
 
-FileList = dir('../Outputs/OutputsIcR/* . mat');
+FileList = dir('../Outputs/OutputsIcR/*.mat');
 
-% Store measurements (PoplationCell 1, Pop . Cell 2, relative Chi,
-% relative i)
+%Store measurements (PoplationCell 1, Pop. Cell 2, relative Chi,
+%relative i)
 Measurements = zeros(length(FileList), 6); 
 
 for ii = 1:length(FileList)
-    File = ['../Outputs/OutputsIcR/', FileList (ii) . name];
+    File = ['../Outputs/OutputsIcR/', FileList(ii).name];
     load(File)
 
     %% Get wave at last timepoint
-    rho_cell _end = rho_cell _store(end, :);
-    rho_cell2 _end = rho_cell2 _store(end, :);
+    rho_cell_end = rho_cell_store(end, :);
+    rho_cell2_end = rho_cell2_store(end, :);
 
     % set(gca', 'yscale', 'log')
 
     %% Get indices of the wave
-    % Find the peaks and start of wave 1
-    peaks = findpeaks(rho_cell _end);
-    peakInd = find(rho_cell _end == peaks(end));
-    WaveStartInd = max(find(rho_cell _end(1:peakInd) <= min(rho_cell _end(1:peakInd))));
+    %Find the peaks and start of wave 1
+    peaks = findpeaks(rho_cell_end);
+    peakInd = find(rho_cell_end == peaks(end));
+    WaveStartInd = max(find(rho_cell_end(1:peakInd) <= min(rho_cell_end(1:peakInd))));
 
-    % Find peaks and start of wave 2
-    peaks2 = findpeaks(rho_cell2 _end);
-    peakInd2 = find(rho_cell2 _end == peaks2(end));
-    WaveStartInd2 = max(find(rho_cell2 _end(1:peakInd2) <= min(rho_cell2 _end(1:peakInd))));
+    %Find peaks and start of wave 2
+    peaks2 = findpeaks(rho_cell2_end);
+    peakInd2 = find(rho_cell2_end == peaks2(end));
+    WaveStartInd2 = max(find(rho_cell2_end(1:peakInd2) <= min(rho_cell2_end(1:peakInd))));
 
 %     figure();
-%     plot(rho_cell _end)
+%     plot(rho_cell_end)
 %     hold on
 %     xline(peakInd)
 %     xline(WaveStartInd)
 % 
-%     plot(rho_cell2 _end)
+%     plot(rho_cell2_end)
 %     xline(peakInd2)
 %     xline(WaveStartInd2)
 
     %% Sum cell density in the wave
-    cell_population = sum(rho_cell _end(WaveStartInd:end));
-    cell2_population = sum(rho_cell2 _end(WaveStartInd:end));
+    cell_population = sum(rho_cell_end(WaveStartInd:end));
+    cell2_population = sum(rho_cell2_end(WaveStartInd:end));
     
     %% Get Parameters
-    relativecR = SimParams . cR2 ./ SimParams . cR;
-    RelativeI = SimParams . irate2 ./ SimParams . irate;
+    relativecR = SimParams.cR2 ./ SimParams.cR;
+    RelativeI = SimParams.irate2 ./ SimParams.irate;
     
     %% Store measurements
-    Measurements(ii, :) = [cell_population, cell2_population, relativecR, RelativeI, SimParams . cR, SimParams . irate];
+    Measurements(ii, :) = [cell_population, cell2_population, relativecR, RelativeI, SimParams.cR, SimParams.irate];
 end
 
 %% Plot
 relcR = Measurements(:, 3);
 relI = Measurements(:, 4);
-relPopulation = Measurements (:, 2) ./ Measurements(:, 1);
+relPopulation = Measurements(:, 2) ./ Measurements(:, 1);
 
 
 figure()
 scatter(relcR, relI, 60, log(relPopulation), 'filled')
 a = colorbar;
 colormap jet
-a . Label . String = 'log N1/N2';
-a . Label . Rotation = -90;
+a.Label.String = 'log N1/N2';
+a.Label.Rotation = -90;
 xlabel("Relative cR")
 ylabel("Relative I")
 set(gca,'xscale', 'log', 'yscale', 'log')
-saveas(gcf, ' ./I_vs_cR . png')
+saveas(gcf, './I_vs_cR.png')
 
 %% Export Data
-headers = ["Cell_population", "Cell2_population", "relativecR", "relativeI", "cR", "irate"];
+headers = ["Cell_population", "Cell2_population", "relativecA", "relativeI", "cR", "irate"];
 textHeader = strjoin(headers, ',');
-fid = fopen(' ./I_vs_cR . csv', 'w');
-fprintf(fid,'% s \n',textHeader)
+fid = fopen('./I_vs_cR.csv', 'w');
+fprintf(fid,'%s\n',textHeader)
 fclose(fid)
 
-dlmwrite(' ./I_vs_cR . csv',Measurements,'-append');
+dlmwrite('./I_vs_cR.csv',Measurements,'-append');
