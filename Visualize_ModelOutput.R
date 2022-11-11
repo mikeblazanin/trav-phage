7dfcb55fd35c76903673f2a0ad7af72ea7da9d35
mikeblazanin <mikeblazanin@gmail.com>
2022-11-11 00:48:10 -0500

    Fig tweaks; figure specially tuned for main text

--- Visualize_ModelOutput.R
+++ Visualize_ModelOutput.R
@@ -1,4 +1,5 @@
 library(ggplot2)
+library(ggtext)
 
 #Read in modeling results data
 data <- lapply(
@@ -50,6 +51,9 @@ library(dplyr)
 library(purrr)
 data_mrg <- data %>% reduce(full_join)
 data_mrg$Cell2_Cell1 <- data_mrg$Cell2_population/data_mrg$Cell_population
+data_mrg$distrib <- 
+  factor(data_mrg$distrib,
+         levels = c("no_paras", "local", "global", "global_gauss"))
 
 #Make plots
 for (vars_manip in unique(data_mrg$vars_manip)) {
@@ -60,17 +64,17 @@ for (vars_manip in unique(data_mrg$vars_manip)) {
   mybreaks <- pretty(x = log10(my_data$Cell2_Cell1), n = 6)
   
   #axis labels labeller for the non-resistance axis
-  mylabeller <-
+  axislabeller <-
     as_labeller(c(relativecA = "Relative Attractant Consumption",
                   relativeChi = "Relative Chemotactic Sensitivity",
                   relativecR = "Relative Growth Rate",
                   relativeY = "Relative Growth Yield"))
   
   #facet labels labeller
-  mylabller <-
-    as_labeller(c(global = "Global Parasite Distribution",
-                  local = "Local Parasite Distribution",
-                  global_gauss = "Global Parasite Distribution (Gaussian)",
+  facetlabeller <-
+    as_labeller(c(global = "Global Parasites",
+                  local = "Local Parasites",
+                  global_gauss = "Global Parasites (Gaussian)",
                   no_paras = "No Parasites"))
   
   #Make file
@@ -78,7 +82,7 @@ for (vars_manip in unique(data_mrg$vars_manip)) {
     paste(sep = "", "./Model_plots/", vars_manip, ".png"),
     width = 6, height = 5, units = "in", res = 150)
   #Make plot
-  print(
+  p <-
     ggplot(my_data,
            aes_string(x = "relativeR", 
                       y = paste("relative", my_data$vars_manip_2[1], sep = ""))) +
@@ -86,109 +90,62 @@ for (vars_manip in unique(data_mrg$vars_manip)) {
                           breaks = mybreaks) +
       geom_point(aes(color = log10(Cell2_population/Cell_population)),
                  size = 3) +
-      facet_wrap(~distrib) +
-      scale_x_continuous(trans = "log10") +
-      scale_y_continuous(trans = "log2") +
+      facet_wrap(~distrib, labeller = facetlabeller) +
+      scale_x_continuous(trans = "log10",
+                         breaks = 10**c(-2, -1, 0, 1, 2),
+                         labels = c("0.01", "0.1", "1", "10", "100")) +
       scale_fill_viridis_d(drop = FALSE) +
       scale_color_continuous(name = "Fitness", type = "viridis",
                              limits = c(min(mybreaks), max(mybreaks))) +
-      #guides(fill = "none") +
-      # labs(subtitle = mylabel,
-      #      x = "Relative Resistance",
-      #      y = mylabeller(grep("relative[^IR]+", colnames(data[[i]]),
-      #                          value = TRUE))[[1]]) +
+      guides(fill = "none") +
+      labs(x = "Relative Resistance") +
       theme_bw() +
       NULL
-  )
+  if(vars_manip == "I_vs_Y") {
+    p <- p + 
+      scale_y_continuous(trans = "log2", breaks = 2**c(-.5, 0, .5),
+                         labels = c("-0.5", "0", "0.5"),
+                         name = "log<sub>2</sub>(Relative Growth Yield)") +
+      theme(axis.title.y = element_markdown())
+  } else {p <- p + 
+    scale_y_continuous(
+      trans = "log2",
+      name = axislabeller(paste("relative", my_data$vars_manip_2[1], sep = "")))
+  }
+  print(p)
   dev.off()
   
-  ##Now points only (no landscape)
-  #Make file
-  # png(
-  #   paste(sep = "", "./Model_plots/", vars_pattern, mylabel, 
-  #         "_pointsonly.png"),
-  #   width = 6, height = 5, units = "in", res = 150)
-  # 
-  # #Make plot
-  # print(
-  #   ggplot(data[[i]],
-  #          aes_string(x = "relativeR",
-  #                     y = grep("relative[^IR]+", colnames(data[[i]]),
-  #                              value = TRUE))) +
-  #     # geom_contour_filled(aes(z = log10(Cell2_population/Cell_population)),
-  #     #                     breaks = mybreaks) +
-  #     geom_point(aes(color = log10(Cell2_population/Cell_population)),
-  #                size = 3) +
-  #     scale_x_continuous(trans = "log10") +
-  #     scale_y_continuous(trans = "log10") +
-  #     scale_fill_viridis_d(drop = FALSE) +
-  #     scale_color_continuous(name = "Fitness", type = "viridis",
-  #                            limits = c(min(mybreaks), max(mybreaks))) +
-  #     guides(fill = "none") +
-  #     labs(subtitle = mylabel,
-  #          x = "Relative Resistance",
-  #          y = mylabeller(grep("relative[^IR]+", colnames(data[[i]]),
-  #                              value = TRUE))[[1]]) +
-  #     theme_bw() +
-  #     NULL
-  # )
-  # dev.off()
-  
-  #Plot change in fitness landscape of local relative to global
-  
-  #First calculate change in frequency bt two landscapes
-  # glob_idx <- grep("uniform", names(data)[these_vars_idx])
-  # loc_idx <- grep("gPhage", names(data)[these_vars_idx])
-  # 
-  # glob_temp <- data[[these_vars_idx[glob_idx]]]
-  # glob_temp$inv_fitness_glob <- 
-  #   glob_temp$Cell2_population/glob_temp$Cell_population
-  # 
-  # loc_temp <- data[[these_vars_idx[loc_idx]]]
-  # loc_temp$inv_fitness_loc <-
-  #   loc_temp$Cell2_population/loc_temp$Cell_population
-  # 
-  # #Drop extra columns
-  # glob_temp  <- 
-  #   glob_temp[, 
-  #             c(match(c("relativeR", "inv_fitness_glob"),
-  #                     colnames(glob_temp)),
-  #               grep("relative[^IR]+", colnames(glob_temp)))]
-  # loc_temp  <- 
-  #   loc_temp[, 
-  #             c(match(c("relativeR", "inv_fitness_loc"),
-  #                     colnames(loc_temp)),
-  #               grep("relative[^IR]+", colnames(loc_temp)))]
-  # 
-  # temp <- dplyr::full_join(glob_temp, loc_temp)
-  # temp$delta_freq <- temp$inv_fitness_glob - temp$inv_fitness_loc
-  # 
-  # #Make file
-  # png(
-  #   paste(sep = "",
-  #         "./Model_plots/",
-  #         paste(collapse = "_", strsplit(names(data)[i], split = "_")[[1]][1:3]), 
-  #         "_delta.png"),
-  #   width = 6, height = 5, units = "in", res = 150)
-  # #Make plot
-  # print(
-  #   ggplot(temp,
-  #          aes_string(x = "relativeR",
-  #                     y = grep("relative[^IR]+", colnames(temp),
-  #                              value = TRUE))) +
-  #     geom_contour_filled(aes(z = delta_freq)) +
-  #     #geom_point(aes(color = delta_freq), size = 3) +
-  #     scale_x_continuous(trans = "log10") +
-  #     scale_y_continuous(trans = "log10") +
-  #     scale_fill_brewer(type = "div") +
-  #     #scale_color_gradient2(name = "Change in Fitness") +
-  #     #guides(fill = FALSE) +
-  #     labs(x = "Relative Resistance",
-  #          y = mylabeller(grep("relative[^IR]+", colnames(temp),
-  #                              value = TRUE))[[1]]) +
-  #     theme_bw() +
-  #     NULL
-  # )
-  # dev.off()
+  #Make main-text figure
+  if(vars_manip == "I_vs_Chi") {
+    myrows <- which(data_mrg$vars_manip == vars_manip &
+                      data_mrg$distrib %in% c("local", "global", "no_paras"))
+    my_data <- data_mrg[myrows, ]
+    
+    #Get limits for log10(Cell2/Cell) across the dift initial distributions
+    mybreaks <- pretty(x = log10(my_data$Cell2_Cell1), n = 6)
+    
+    #Make plot
+    png("./Model_plots/I_vs_Chi_maintext.png", width = 8, height = 3, units = "in", res = 150)
+    print(
+      ggplot(my_data, aes_string(x = "relativeR", y = "relativeChi")) +
+        geom_contour_filled(aes(z = log10(Cell2_population/Cell_population)),
+                            breaks = mybreaks) +
+        geom_point(aes(color = log10(Cell2_population/Cell_population)),
+                   size = 3) +
+        facet_grid(~distrib, labeller = facetlabeller) +
+        scale_x_continuous(trans = "log10",
+                           breaks = 10**c(-2, -1, 0, 1, 2),
+                           labels = c("0.01", "0.1", "1", "10", "100")) +
+        scale_y_continuous(trans = "log2") +
+        scale_fill_viridis_d(drop = FALSE) +
+        scale_color_continuous(name = "Fitness", type = "viridis",
+                               limits = c(min(mybreaks), max(mybreaks))) +
+        guides(fill = "none") +
+        labs(x = "Relative Resistance", y = "Relative Dispersal") +
+        theme_bw() +
+        NULL
+    )
+    dev.off()
+  }
 }
 
